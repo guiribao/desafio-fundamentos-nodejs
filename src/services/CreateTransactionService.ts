881633d6678af0fb1ceced9f35684d86e6afc0b6
Guilherme Rosa <guilherme.rosa@synchro.com.br>
2021-12-20 23:44:51 -0300

    Solução para o desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionDTO): Transaction {
+    if(!["income", "outcome"].includes(type)) {
+      throw new Error("Tipo de transação inválido");
+    }
+
+    const { total } = this.transactionsRepository.getBalance()
+
+    if(type === "outcome" && total < value) {
+      throw new Error("Você não tem saldo suficiente");
+    }
+
+    let transaction = this.transactionsRepository.create({ title, value, type })
+
+    return transaction;
   }
 }
 
